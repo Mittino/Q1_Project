e42ff12e742ff2db7972c1d6561eb6146a5dacc7
Anna Mefford <annamefford@gmail.com>
2016-10-30 11:27:02 -0600

    location adding to data obj

--- adoptions.js
+++ adoptions.js
@@ -1,34 +1,48 @@
 "use strict";
-
-
-
+var apiKey = "7e3c2eb42f00b573ab85a0e4f1d4a9ca";
 
 $(document).ready(function(){
 
-$("#search").on('submit', function(event){
-  event.preventDefault();
-  console.log('hello');
-  getResults();
-});
+  $("#search").on('submit', function(event){
+    event.preventDefault();
+    console.log('hello');
+    getResults();
+  });
+
+  $('select').material_select();
 
 
 });
 
-var searchLocation = '80302';
 
 function getResults(){
 
-$.ajax({
-  url:'http://api.petfinder.com/pet.find?format=json&key=7e3c2eb42f00b573ab85a0e4f1d4a9ca&dog&' + searchLocation + '&output=full&format=json',
-  jsonp: "callback",
-  dataType:"jsonp",
-  data: {
+  var data = {
     format: "json",
-  },
-  type: 'get',
-  success: function(){
-    console.log('success');
+    output: "full",
+    key: apiKey,
+    animal: "dog",
+  };
 
+  var searchInput = $('#location').val();
+  var zipCode = searchInput.replace(/\D/g, '');
+  console.log(zipCode);
+  if (zipCode.length < 5 || zipCode.length > 5){
+    console.log('enter complete zip code');
+  } else if (zipCode.length === 5){
+    data.location = zipCode;
   }
-});
+
+  console.log(data);
+
+  // $.ajax({
+  //   url:'http://api.petfinder.com/pet.find' + searchLocation,
+  //   jsonp: "callback",
+  //   dataType:"jsonp",
+  //   data: data,
+  //   type: 'get',
+  //   success: function(data){
+  //     console.log(data);
+  //   }
+  // });
 }
