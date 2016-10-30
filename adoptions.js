ecae4a12c99bdee44f619f4e406d06bd0b3385c8
Anna Mefford <annamefford@gmail.com>
2016-10-30 09:05:44 -0600

    event listener on button and API functionality

--- adoptions.js
+++ adoptions.js
@@ -1,9 +1,34 @@
 "use strict";
 
-(function(){
 
 
 
+$(document).ready(function(){
+
+$("#search").on('submit', function(event){
+  event.preventDefault();
+  console.log('hello');
+  getResults();
+});
 
 
-})();
+});
+
+var searchLocation = '80302';
+
+function getResults(){
+
+$.ajax({
+  url:'http://api.petfinder.com/pet.find?format=json&key=7e3c2eb42f00b573ab85a0e4f1d4a9ca&dog&' + searchLocation + '&output=full&format=json',
+  jsonp: "callback",
+  dataType:"jsonp",
+  data: {
+    format: "json",
+  },
+  type: 'get',
+  success: function(){
+    console.log('success');
+
+  }
+});
+}
