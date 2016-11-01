6a224004d59d596a672813e2dc3cf8f3e7a83289
Anna Mefford <annamefford@gmail.com>
2016-11-01 16:17:57 -0600

    name phone dogname shelter validation on form

--- AdoptionForm.js
+++ AdoptionForm.js
@@ -8,16 +8,24 @@
     $('#adoptionForm').on('submit', function(event){
       event.preventDefault();
       submitApplication();
-      console.log("clicked");
     });
 
   });
 
 
   function submitApplication(){
-    console.log($("#phone").val());
-    if ($("#phone").val().replace(/\D/g, '').length !== 10){
+    var name = $('#name').val();
+    console.log(name.length);
+    if (name.length === 0){
+      Materialize.toast('Please Enter Your Full Name', 4000);
+    }
+    else if ($("#phone").val().replace(/\D/g, '').length !== 10){
       Materialize.toast('Please Enter a Valid Phone Number', 4000);
+    } else if ($("#dogname").val().length === 0){
+      Materialize.toast('Please Enter a Valid Dog Name', 4000);
+    }
+    else if ($("#shelterId").val().length !== 5){
+      Materialize.toast('Please Enter a Valid Shelter Id', 4000);
     }
     else Materialize.toast('Thank you for your application!', 4000);
   }
