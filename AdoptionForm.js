fbe1a2e84f9a038cce8c48374fedb620106526bc
Anna Mefford <annamefford@gmail.com>
2016-11-03 16:38:44 -0600

    Mobile Responsiveness added to all pgs

--- AdoptionForm.js
+++ AdoptionForm.js
@@ -4,7 +4,7 @@
 
   $(document).ready(function(){
     $('select').material_select();
-
+    $(".button-collapse").sideNav();
     $('#adoptionForm').on('submit', function(event){
       event.preventDefault();
       submitApplication();
